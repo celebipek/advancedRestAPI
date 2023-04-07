5d6993d57b2ac7bb780602f93aea29088c7d1561
Celebi Ipek <celebiipek870@gmail.com>
2023-04-07 15:29:13 +0300

    FinishedRESTApi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./config/database.js')
 const Auth = require('./routes/auth.js')
+const Post = require('./routes/post.js')
 
 dotenv.config();
 
@@ -12,10 +13,12 @@ app.use(express.json({limit:'30mb', extended: true}));
 app.use(express.urlencoded({limit:'30mb', extended: true}));
 
 app.use('/', Auth)
+app.use('/', Post)
 
-app.get('/', (req, res)=>{
-    res.json({message:"deneme deneme"})
-})
+
+// app.get('/', (req, res)=>{
+//     res.json({message:"deneme deneme"})
+// })
 
 const PORT = process.env.PORT || 5000;
 
